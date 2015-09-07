90b49f6a73fb58a7e12644c199bf65d2a3554a3a
Shingyu Kang <askthecode@gmail.com>
2015-09-07 11:09:36 +0900

    Change qnx build scripts to load qnx660-env.sh in case QNX_TARGET is not set

--- build-qnx-arm.sh
+++ build-qnx-arm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ ! -f "${HOME}/qnx660/qnx660-env.sh" ]; then
-  echo "Can not find ${HOME}/qnx660/qnx660-env.sh"
-  exit 1
+if [ "x${QNX_TARGET}" == "x" ]; then
+  if [ ! -f "${HOME}/qnx660/qnx660-env.sh" ]; then
+    echo "Can not find ${HOME}/qnx660/qnx660-env.sh"
+    exit 1
+  fi
+  source ~/qnx660/qnx660-env.sh
 fi
 
-source ~/qnx660/qnx660-env.sh
-
-./configure --crossbuild=QNX:6.6.0:arm --with-cc="qcc -V4.7.3,gcc_ntoxarmv7le" --with-ld-opt="-lsocket -llogin" --without-http_rewrite_module
+./configure --crossbuild=QNX:6.6.0:arm --with-cc="qcc -V4.7.3,gcc_ntoarmv7le" --with-ld-opt="-lsocket -llogin" --without-http_rewrite_module
 
 make -j8
--- build-qnx-x86.sh
+++ build-qnx-x86.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ ! -f "${HOME}/qnx660/qnx660-env.sh" ]; then
-  echo "Can not find ${HOME}/qnx660/qnx660-env.sh"
-  exit 1
+if [ "x${QNX_TARGET}" == "x" ]; then
+  if [ ! -f "${HOME}/qnx660/qnx660-env.sh" ]; then
+    echo "Can not find ${HOME}/qnx660/qnx660-env.sh"
+    exit 1
+  fi
+  source ~/qnx660/qnx660-env.sh
 fi
-
-source ~/qnx660/qnx660-env.sh
 
 ./configure --crossbuild=QNX:6.6.0:x86 --with-cc="qcc -V4.7.3,gcc_ntox86" --with-ld-opt="-lsocket -llogin" --without-http_rewrite_module
 
